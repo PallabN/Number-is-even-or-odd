b9fd7d5345f2852deb9a46e47d10a0b09f9a7a7f
PallabN <94918550+PallabN@users.noreply.github.com>
2021-11-24 20:17:32 +0530

    Add files via upload

---
+++ Number is Even or Odd  (2nd  sem).cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int a;
+printf("\n\t\t*****EVEN ODD CHECKING*****\n");
+printf("\n\tEnter the value of a:");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("\n\ta is an evev number");
+}
+else
+{
+printf("\n\ta is an odd number");
+}
+return(0);
+}
